175f021d4884619edf892916bfadc774eb863e0e
Jarred Capellman <jcapellman@hotmail.com>
2022-06-04 12:26:31 -0500

    Added: Initial Output Format Type

---
+++ EDR.lib/OutputFormatTypes/Base/BaseOutputFormatType.cs
@@ -0,0 +1,11 @@
+﻿using WET.lib.Containers;
+
+namespace EDR.Collector.lib.OutputTypes.Base
+{
+    public abstract class BaseOutputFormatType
+    {
+        public abstract string Name { get; }
+
+        public abstract string Format(ETWEventContainerItem item);
+    }
+}
