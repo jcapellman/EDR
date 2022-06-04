78fc0f1d3fa7d4ca239da8ac343a49d16ec17edf
Jarred Capellman <jcapellman@hotmail.com>
2022-06-04 12:27:23 -0500

    Added: Initial work on the BaseStorageType class

---
+++ EDR.lib/StorageTypes/Base/BaseStorageType.cs
@@ -0,0 +1,9 @@
+﻿namespace EDR.Collector.lib.StorageTypes.Base
+{
+    public abstract class BaseStorageType
+    {
+        public abstract string Name { get; }
+
+        public abstract bool StoreEvent(string output);
+    }
+}
