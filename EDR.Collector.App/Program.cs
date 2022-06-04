7e5beea55d784cc4d819c20e1f818cb1ced020aa
Jarred Capellman <jcapellman@hotmail.com>
2022-06-04 12:30:48 -0500

    Change: Updated file paths

---
+++ EDR.Collector.App/Program.cs
@@ -0,0 +1,26 @@
+﻿using EDR.Collector.lib;
+using EDR.Collector.lib.Helpers;
+
+namespace EDR.Collector.App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ManualResetEvent mreShutdown = new(false);
+
+            var collector = new CollectorRunner(ConfigParser.LoadConfig(args.Length == 0 ? null : args[0]));
+
+            Console.CancelKeyPress += (sender, eventArgs) => {
+                eventArgs.Cancel = true;
+                mreShutdown.Set();
+            };
+
+            collector.Start();
+
+            mreShutdown.WaitOne();
+
+            collector.Stop();
+        }
+    }
+}
