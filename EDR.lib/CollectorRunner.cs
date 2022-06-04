4a0b49f061828db5d7db81d4edbf2f4f4849cf55
Jarred Capellman <jcapellman@hotmail.com>
2022-06-04 12:26:03 -0500

    Added: Initial work on the CollectorRunner class

---
+++ EDR.lib/CollectorRunner.cs
@@ -0,0 +1,65 @@
+﻿using EDR.Collector.lib.Objects;
+using EDR.Collector.lib.OutputFormatTypes;
+using EDR.Collector.lib.OutputTypes.Base;
+using EDR.Collector.lib.StorageTypes;
+using EDR.Collector.lib.StorageTypes.Base;
+
+using WET.lib;
+
+namespace EDR.Collector.lib
+{
+    public class CollectorRunner
+    {
+        private readonly Config _config;
+        private readonly ETWMonitor _wet = new();
+
+        private BaseOutputFormatType _outputFormatter;
+        private BaseStorageType _storage;
+
+        public CollectorRunner(Config config)
+        {
+            _config = config;
+
+            _outputFormatter = InitializeOutputFormat(_config.OutputFormat);
+            _storage = InitializeStorageType(_config.StorageType, _config.StorageTypeConfig);
+        }
+
+        private BaseOutputFormatType InitializeOutputFormat(string formatType)
+        {
+            // TODO: Use reflection to load output formatters and return
+            return new SysLog();
+        }
+
+        private BaseStorageType InitializeStorageType(string storageType, string storageTypeConfig)
+        {
+            // TODO: Use reflection to load storage types and return
+            return new AWSS3Storage();
+        }
+
+        public void Start()
+        {
+            _wet.OnEvent += Wet_OnEvent;
+
+            _wet.Start();
+        }
+
+        public void Stop()
+        {
+            _wet.OnEvent -= Wet_OnEvent;
+
+            _wet.Stop();
+        }
+
+        private void Wet_OnEvent(object? sender, WET.lib.Containers.ETWEventContainerItem e)
+        {
+            var outputStr = _outputFormatter.Format(e);
+
+            if (_config.EnableStdOutToConsole)
+            {
+                Console.WriteLine(outputStr);
+            }
+
+            _storage.StoreEvent(outputStr);
+        }
+    }
+}
