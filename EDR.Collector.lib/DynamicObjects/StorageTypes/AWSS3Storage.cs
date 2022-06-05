b05f60610d3d688f176ff110c4fac9b5a2b56e8d
Jarred Capellman <jcapellman@hotmail.com>
2022-06-05 07:47:46 -0500

    Change: Cleaned up the AWS Class

---
+++ EDR.Collector.lib/DynamicObjects/StorageTypes/AWSS3Storage.cs
@@ -0,0 +1,16 @@
+﻿using EDR.Collector.lib.DynamicObjects.StorageTypes.Base;
+
+namespace EDR.Collector.lib.DynamicObjects.StorageTypes
+{
+    public class AWSS3Storage : BaseStorageType
+    {
+        public override string Name => "AWS S3";
+
+        public override bool StoreEvent(string output)
+        {
+            //TODO: Actually store in an S3 bucket
+
+            return true;
+        }
+    }
+}
---
+++ EDR.Collector.lib/DynamicObjects/StorageTypes/Base/BaseStorageType.cs
@@ -0,0 +1,9 @@
+﻿using EDR.Collector.lib.DynamicObjects.Base;
+
+namespace EDR.Collector.lib.DynamicObjects.StorageTypes.Base
+{
+    public abstract class BaseStorageType : BaseDynamicObject
+    {
+        public abstract bool StoreEvent(string output);
+    }
+}
