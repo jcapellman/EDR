8fd666e3513997547d89847dd12d5c70f454ded2
Jarred Capellman <jcapellman@hotmail.com>
2022-06-05 07:52:59 -0500

    Change: Implemented the Base class for the AWS class

--- EDR.Collector.lib/DynamicObjects/StorageTypes/AWSS3Storage.cs
+++ EDR.Collector.lib/DynamicObjects/StorageTypes/AWSS3Storage.cs
@@ -6,6 +6,13 @@ namespace EDR.Collector.lib.DynamicObjects.StorageTypes
     {
         public override string Name => "AWS S3";
 
+        public override bool Initialize(string configStr)
+        {
+            //TODO: Initialize the S3 connector
+
+            return true;
+        }
+
         public override bool StoreEvent(string output)
         {
             //TODO: Actually store in an S3 bucket
