68b68c152e42388c699e5e3ebb5faaa5e287e9cb
Jarred Capellman <jcapellman@hotmail.com>
2022-06-05 07:48:01 -0500

    Added: Dynamic Loader

---
+++ EDR.Collector.lib/Helpers/DynamicLoader.cs
@@ -0,0 +1,19 @@
+﻿using EDR.Collector.lib.DynamicObjects.Base;
+
+namespace EDR.Collector.lib.Helpers
+{
+    public static class DynamicLoader
+    {
+        public static T? InitializeGeneric<T>(string objectName) where T : BaseDynamicObject
+        {
+            var type = typeof(DynamicLoader).Assembly.GetTypes().FirstOrDefault(a => a == typeof(T) && a.Name == objectName && !a.IsAbstract);
+
+            if (type == null)
+            {
+                return null;
+            }
+
+            return (T?)Activator.CreateInstance(type);
+        }
+    }
+}
