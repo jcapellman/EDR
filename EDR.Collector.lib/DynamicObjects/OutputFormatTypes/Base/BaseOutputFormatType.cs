eb50cf723078d1bc75614084f02580e86084ff38
Jarred Capellman <jcapellman@hotmail.com>
2022-06-05 07:47:27 -0500

    Change: Cleaned up the SysLog class

---
+++ EDR.Collector.lib/DynamicObjects/OutputFormatTypes/Base/BaseOutputFormatType.cs
@@ -0,0 +1,11 @@
+﻿using EDR.Collector.lib.DynamicObjects.Base;
+
+using WET.lib.Containers;
+
+namespace EDR.Collector.lib.DynamicObjects.OutputFormatTypes.Base
+{
+    public abstract class BaseOutputFormatType : BaseDynamicObject
+    {
+        public abstract string Format(ETWEventContainerItem item);
+    }
+}
---
+++ EDR.Collector.lib/DynamicObjects/OutputFormatTypes/SysLog.cs
@@ -0,0 +1,12 @@
+﻿using EDR.Collector.lib.DynamicObjects.OutputFormatTypes.Base;
+using WET.lib.Containers;
+
+namespace EDR.Collector.lib.DynamicObjects.OutputFormatTypes
+{
+    public class SysLog : BaseOutputFormatType
+    {
+        public override string Name => "SysLog";
+
+        public override string Format(ETWEventContainerItem item) => $"<1>{item.Timestamp.ToString("MMM dd H:m:ss")} {item.hostname} {item.MonitorType}:{item.Payload}";
+    }
+}
