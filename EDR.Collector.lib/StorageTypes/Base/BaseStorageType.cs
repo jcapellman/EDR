0519771c2484ec12251ab11c3fa32f4c2fc7d1c9
Jarred Capellman <jcapellman@hotmail.com>
2022-06-05 07:48:30 -0500

    Change: Updated the folder structure

--- EDR.Collector.lib/StorageTypes/Base/BaseStorageType.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EDR.Collector.lib.StorageTypes.Base
-{
-    public abstract class BaseStorageType
-    {
-        public abstract string Name { get; }
-
-        public abstract bool StoreEvent(string output);
-    }
-}
