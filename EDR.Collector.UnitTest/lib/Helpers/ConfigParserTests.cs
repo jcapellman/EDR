83cab72b02b4706d442f25b523dfaeaa09ec688f
Jarred Capellman <jcapellman@hotmail.com>
2022-06-12 12:36:51 -0500

    Added: Initial ConfigParser Test

---
+++ EDR.Collector.UnitTest/lib/Helpers/ConfigParserTests.cs
@@ -0,0 +1,14 @@
+﻿namespace EDR.Collector.UnitTest.lib.Helpers
+{
+    [TestClass]
+    public class ConfigParserTests
+    {
+        [TestMethod]
+        public void EmptyCheck()
+        {
+            var result = EDR.Collector.lib.Helpers.ConfigParser.LoadConfig(string.Empty);
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
