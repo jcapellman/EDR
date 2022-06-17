ce7af66b0cb70d6846b01623b82d37f057d54e17
Jarred Capellman <jcapellman@hotmail.com>
2022-06-17 16:37:46 -0500

    Added: Local Storage Config Unit Test

--- EDR.Collector.UnitTest/lib/Helpers/ConfigParserTests.cs
+++ EDR.Collector.UnitTest/lib/Helpers/ConfigParserTests.cs
@@ -1,4 +1,9 @@
-﻿namespace EDR.Collector.UnitTest.lib.Helpers
+﻿using EDR.Collector.lib.DynamicObjects.StorageTypes;
+using EDR.Collector.lib.Objects;
+using System.Text.Json;
+using static EDR.Collector.lib.DynamicObjects.StorageTypes.LocalStorage;
+
+namespace EDR.Collector.UnitTest.lib.Helpers
 {
     [TestClass]
     public class ConfigParserTests
@@ -7,6 +12,33 @@
         public void EmptyCheck()
         {
             var result = EDR.Collector.lib.Helpers.ConfigParser.LoadConfig(string.Empty);
+
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void InitializedLocalStorage()
+        {
+            var localStorage = new LocalStorage();
+
+            var localStorageConfig = new LocalStorageConfig
+            {
+                FilePath = AppContext.BaseDirectory               
+            };
+
+            var json = JsonSerializer.Serialize(localStorageConfig);
+
+            var config = new Config();
+
+            config.StorageType = "LocalStorage";
+            config.StorageTypeConfig = json;
+            config.OutputFormat = "syslog";
+
+            var filePath = Path.Combine(AppContext.BaseDirectory, "config.json");
+
+            File.WriteAllText(filePath, JsonSerializer.Serialize(config));
+
+            var result = EDR.Collector.lib.Helpers.ConfigParser.LoadConfig(filePath);
 
             Assert.IsNotNull(result);
         }
