f32c89d73f133123b57a73b3aab19ad329940308
Jarred Capellman <jcapellman@hotmail.com>
2022-06-16 19:40:21 -0500

    Added: Additional coverage

---
+++ EDR.Collector.UnitTest/lib/CollectorRunnerTests.cs
@@ -0,0 +1,21 @@
+﻿using EDR.Collector.lib;
+
+namespace EDR.Collector.UnitTest.lib
+{
+    [TestClass]
+    public class CollectorRunnerTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(UnauthorizedAccessException))]
+        public void EmptyConstructorTest()
+        {
+            var runner = new CollectorRunner();
+
+            runner.Start();
+
+            Task.Delay(500);
+
+            runner.Stop();
+        }
+    }
+}
